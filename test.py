59e9e65216a258d34809a5d3267d45d664a21364
SusieShaooo <shaozijing0830@hotmail.com>
2026-01-28 21:37:33 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+def two_sum(nums, target):
+    num_map = {}
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], index]
+        num_map[num] = index
+    return []
+
+
+
+
